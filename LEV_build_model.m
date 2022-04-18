1067fb9a337a2fe19732a808165b1d1a2730a2d0
skulka13 <89809022+skulka13@users.noreply.github.com>
2022-04-18 15:16:09 -0400

    Add files via upload

---
+++ LEV_build_model.m
@@ -0,0 +1,189 @@
+clear all;
+close all;
+
+%% STEP 1:
+%Build model, mass/mole balance
+
+%% PARAMETERS
+q = 0;     % units: nmol/hr
+V = 42; % units: L (volume of distribution)
+kA  =  0.464; % units: 1/hr (absorption rate constant)
+kCL = 0.096; %units: 1/hr (clearance rate constant)
+IC50 = 2.43; %mg/L
+Kd = 1.3617; % units: mg/L
+MISSED = 0; %for missed dose analysis, not relevant here
+
+%% Single dose, 1000 mg (to compare to plot from paper)
+Dose = 1000; %mg
+TimeLen = 14; %hours between doses
+MASS_BAL_VIS = 1; %Set to 1 to visualize mass balance
+DOSEFREQ = 0; %Set to 0 for single dose, 1 for repeated dosing
+
+%Run simulation for single dose and print concentrations, amounts, and mass balance
+[Conc,Time,AUC0,Ctrough0,Receptor,Effect,P_tonic,P_clonic] = Levetiracetam_sim(kA,V,kCL,Dose,TimeLen,q,IC50,Kd,MASS_BAL_VIS,DOSEFREQ,MISSED);
+
+% Plot single dose to compare to paper figure
+figure;
+plot(Time, Conc(:,1), 'linewidth', 3);
+title('Concentration of Levetiracetam in Compartment: Single Dose', 'FontSize', 16);
+ylabel('[D] (mg/L)', 'FontSize', 12);
+xlabel('Time (hrs)', 'FontSize', 12);
+ylim([0 50]);
+
+%Plot receptor occupancy for single dose using R/Rmax model
+figure;
+plot(Time, Receptor, 'linewidth', 3);
+title('SV2A Protein Receptor Occupancy by LEV After Single Dose', 'FontSize', 16);
+ylabel('% Occupied (R/Rmax)', 'FontSize', 12);
+xlabel('Time (hrs)', 'FontSize', 12);
+
+%Plot receptor occupancy for single dose using E/Emax model
+figure;
+plot(Time, Effect, 'linewidth', 3);
+title('SV2A Protein Receptor Occupancy by LEV After Single Dose', 'FontSize', 16);
+ylabel('% Occupied (E/Emax)', 'FontSize', 12);
+xlabel('Time (hrs)', 'FontSize', 12);
+
+%Plot protection from tonic seizures
+figure;
+plot(Time, P_tonic, 'linewidth', 3);
+title('Protection Against Tonic Seizures', 'FontSize', 16);
+ylabel('% Protection', 'FontSize', 12);
+xlabel('Time (hrs)', 'FontSize', 12);
+
+%Plot protection from clonic seizures
+figure;
+plot(Time, P_clonic, 'linewidth', 3);
+title('Protection Against Clonic Seizures', 'FontSize', 16);
+ylabel('% Protection', 'FontSize', 12);
+xlabel('Time (hrs)', 'FontSize', 12);
+
+%IF WANT TO PLOT SINGLE DOSE DATA IN R (I DON'T THINK THIS IS NECESSARY)
+%{
+save Conc SingleDoseConc.mat;
+save Time SingleDoseTime.mat;
+save Receptor SingleDoseReceptor.mat;
+save Effect SingleDoseEffect.mat;
+save P_tonic SingleDoseP_tonic.mat;
+save P_clonic SingleDoseP_clonic.mat;
+%}
+
+
+%% Repeated doses, 500 mg every 12 hours
+% PARAMETERS
+Dose = 500; %mg
+TimeLen = 12; %hours between doses
+MASS_BAL_VIS = 1; %Set to 1 to visualize mass balance
+DOSEFREQ = 1; %Set to 0 for single dose, 1 for repeated dosing
+
+%Run simulation for repeated doses and print concentrations, amounts, and mass balance
+[Conc,Time,AUC0,Ctrough0,Receptor,Effect,P_tonic,P_clonic] = Levetiracetam_sim(kA,V,kCL,Dose,TimeLen,q,IC50,Kd,MASS_BAL_VIS,DOSEFREQ,MISSED);
+
+%% STEP 2:
+% Identify and simulate key time-dependent variables for a range of doses
+
+%% SIMULATIONS
+
+% Simulate range of doses for key PK/PD variables: 
+% Concentration in central compartment
+% Receptor occupancy (2 models)
+% Protection from tonic seizures
+% Protection from clonic seizures
+
+% Initialize matrices to store outputs
+Conc = [];
+Time = [];
+Receptor = [];
+Effect = [];
+P_tonic = [];
+P_clonic = [];
+
+%Simulate range of drug doses 250-1500mg
+for i=1:6
+    Dose = 250*i;
+    [Conc1, Time1, AUC(i), Ctrough(i), Receptor1, Effect1, P_tonic1, P_clonic1] = Levetiracetam_sim(kA,V,kCL,Dose,TimeLen,q,IC50,Kd,0,1,0);
+    Conc = [Conc Conc1];
+    Time = [Time Time1'];
+    Receptor = [Receptor Receptor1];
+    Effect = [Effect Effect1];
+    P_tonic = [P_tonic P_tonic1];
+    P_clonic = [P_clonic P_clonic1];
+end
+
+%Print AUC and Ctrough for range of drug doses
+AUC
+Ctrough
+
+%Plot drug concentrations for range of doses
+figure; 
+for i = 1:6
+    hold on;
+    plot(Time(:,i), Conc(:,i), 'linewidth',3);
+end
+    title('Concentration of Levetiracetam in Compartment', 'FontSize', 20);
+    ylabel('[D] (mg/L)', 'FontSize', 16);
+    xlabel('Time (hrs)', 'FontSize', 16);
+    leg = legend('250', '500', '750', '1000', '1250', '1500');
+    title(leg, 'Dose (mg)');
+    
+%Plot receptor occupancy for range of doses with two models
+%R/Rmax model
+figure; 
+ax1=subplot(2,1,1); 
+for i = 1:6
+    hold on;
+    plot(ax1, Time(:,i), Receptor(:,i), 'linewidth',3);
+end
+    title('R/Rmax', 'FontSize', 20);
+    ylabel('% Occupied', 'FontSize', 16);
+    xlabel('Time (hrs)', 'FontSize', 16);
+    leg = legend('250', '500', '750', '1000', '1250', '1500');
+    title(leg, 'Dose (mg)');
+    
+%E/Emax model
+ax2=subplot(2,1,2); 
+for i = 1:6
+    hold on;
+    plot(ax2, Time(:,i), Effect(:,i), 'linewidth',3);
+end
+    title('E/Emax', 'FontSize', 20);
+    ylabel('% Occupied', 'FontSize', 16);
+    xlabel('Time (hrs)', 'FontSize', 16);
+    leg = legend('250', '500', '750', '1000', '1250', '1500');
+    title(leg, 'Dose (mg)');
+    
+%Plot P_tonic for range of doses
+figure; 
+for i = 1:6
+    hold on;
+    plot(Time(:,i), P_tonic(:,i), 'linewidth',3);
+end
+    title('Protection Against Tonic Seizures', 'FontSize', 20);
+    ylabel('Protection (%)', 'FontSize', 16);
+    xlabel('Time (hrs)', 'FontSize', 16);
+    leg = legend('250', '500', '750', '1000', '1250', '1500');
+    title(leg, 'Dose (mg)');
+
+% Plot P_clonic for range of doses
+figure; 
+for i = 1:6
+    hold on;
+    plot(Time(:,i), P_clonic(:,i), 'linewidth',3);
+end
+    title('Protection Against Clonic Seizures', 'FontSize', 20);
+    ylabel('Protection (%)', 'FontSize', 16);
+    xlabel('Time (hrs)', 'FontSize', 16);
+    leg = legend('250', '500', '750', '1000', '1250', '1500');
+    title(leg, 'Dose (mg)');
+
+%Save data for repeated dosing for range of drug doses to import into R
+save Conc DoseRangeConc.mat;
+save Time DoseRangeTime.mat;
+save AUC DoseRangeAUC.mat;
+save Ctrough DoseRangeCtrough.mat;
+save Receptor DoseRangeReceptor.mat;
+save Effect DoseRangeEffect.mat;
+save P_tonic DoseRangeP_tonic.mat;
+save P_clonic DoseRangeP_clonic.mat;
+
+
---
+++ LEV_missed_dose.m
@@ -0,0 +1,113 @@
+%% Missed Dose Analysis
+
+% Repeated doses, 500 mg every 12 hours
+% PARAMETERS
+q = 0;     % units: nmol/hr
+V = 42; % units: L (volume of distribution)
+kA  =  0.464; % units: 1/hr (absorption rate constant)
+kCL = 0.096; %units: 1/hr (clearance rate constant)
+Dose = 500; %mg
+TimeLen = 12; %hours between doses
+MASS_BAL_VIS = 0; %Set to 1 to visualize mass balance
+DOSEFREQ = 2; %Set to 0 for single dose, 1 for repeated dosing, 2 for missed dose
+IC50 = 2.43; %mg/L
+Kd = 1.3617; % units: mg/L
+
+% Choose dose to miss 
+MISSED = 0; 
+% 0 = no missed doses 
+% 1 = missed dose m/5 late
+% 2 = missed dose 2m/5 late
+% 3 = missed dose 3m/5 late
+% 4 = missed dose 4m/5 late
+% 5 = missed dose m late (double dose)
+% 6 = missed dose (skipped entirely)
+
+Conc_missed = [];
+Receptor_missed = [];
+Effect_missed = [];
+Ptonic_missed = [];
+Pclonic_missed = [];
+
+%Missed doses
+for MISSED = 1:6
+[y_m,t_m,auc_m(MISSED),ctrough_m(MISSED),receptor_m,effect_m,P_tonic_m,P_clonic_m] = Levetiracetam_sim(kA,V,kCL,Dose,TimeLen,q,IC50,Kd,0,1,MISSED);
+
+% Calculate Cmin and Cmax for missed doses
+y_trough = y_m(240:length(y_m));
+Cmin(MISSED) = min(y_trough, [], 'all');
+Cmax(MISSED) = max(y_m, [], 'all');
+
+% Plot concentrations
+figure;
+plot(t_m, y_m, 'linewidth', 3);
+if MISSED ~= 6 
+PLOT_title = strcat('Missed Dose Taken', {' '}, string(MISSED*12/5), ' Hours Late');
+    if MISSED == 5
+        PLOT_title = strcat(PLOT_title, ' (Double Dose)');
+    end
+else 
+    PLOT_title = 'Skipped Dose';
+end
+
+title(PLOT_title, 'FontSize', 16);
+ylabel('[D] (mg/L)', 'FontSize', 12);
+xlabel('Time (hrs)', 'FontSize', 12);
+ylim([0 18]);
+
+%Plot tonic seizure protection
+figure;
+plot(t_m, P_tonic_m, 'linewidth', 3);
+if MISSED ~= 6 
+PLOT_title = strcat('Missed Dose Taken', {' '}, string(MISSED*12/5), ' Hours Late');
+    if MISSED == 5
+        PLOT_title = strcat(PLOT_title, ' (Double Dose)');
+    end
+else 
+    PLOT_title = 'Skipped Dose';
+end
+
+title(PLOT_title, 'FontSize', 16);
+ylabel('Protection From Tonic Seizures (%)', 'FontSize', 12);
+xlabel('Time (hrs)', 'FontSize', 12);
+
+%Plot clonic seizure protection
+figure;
+plot(t_m, P_clonic_m, 'linewidth', 3);
+if MISSED ~= 6 
+PLOT_title = strcat('Missed Dose Taken', {' '}, string(MISSED*12/5), ' Hours Late');
+    if MISSED == 5
+        PLOT_title = strcat(PLOT_title, ' (Double Dose)');
+    end
+else 
+    PLOT_title = 'Skipped Dose';
+end
+
+title(PLOT_title, 'FontSize', 16);
+ylabel('Protection From Clonic Seizures (%)', 'FontSize', 12);
+xlabel('Time (hrs)', 'FontSize', 12);
+
+
+%Save concentration and effect data to plot in R
+Conc_missed = [Conc_missed; y_m];
+Receptor_missed = [Receptor_missed; receptor_m];
+Effect_missed = [Effect_missed; effect_m];
+Ptonic_missed = [Ptonic_missed; P_tonic_m];
+Pclonic_missed = [Pclonic_missed; P_clonic_m];
+end
+
+%Print Cmin, Cmax, and AUCs to compare
+% m/5, 2m/5, 3m/5, 4m/5, double dose, skipped dose
+Cmin
+Cmax
+auc_m
+
+%Save data for repeated dosing for range of drug doses to import into R
+save Conc_missed MissedDoseConc.mat;
+save t_m MissedDoseTime.mat;
+save auc_m MissedDoseAUC.mat;
+save ctrough_m MissedDoseCtrough.mat;
+save Receptor_missed MissedDoseReceptor.mat;
+save Effect_missed MissedDoseEffect.mat;
+save P_tonic_missed MissedDoseP_tonic.mat;
+save P_clonic_missed MissedDoseP_clonic.mat;
