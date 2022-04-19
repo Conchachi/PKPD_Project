1b6d869bf167271499fad91b7d67edf53aaaaa94
skulka13 <89809022+skulka13@users.noreply.github.com>
2022-04-19 10:54:48 -0400

    Add files via upload

--- Levetiracetam_sim.m
+++ Levetiracetam_sim.m
@@ -1,4 +1,4 @@
-function [Conc,Time,AUC,Ctrough, R, E, P_tonic, P_clonic] = Levetiracetam_sim(kA, V, kCL, Dose, TimeLen, q, IC50, Kd, MASS_BAL_VIS, DOSEFREQ, MISSED);
+function [Conc,Time,AUC,Ctrough, R, E, P_tonic, P_clonic,AUEC_tonic,E_tonic_trough,AUEC_clonic,E_clonic_trough] = Levetiracetam_sim(kA, V, kCL, Dose, TimeLen, q, IC50, Kd, MASS_BAL_VIS, DOSEFREQ, MISSED);
 
 %% PARAMETERS
 p.q = 0;     % units: nmol/hr
@@ -33,7 +33,7 @@ end
 % MISSED = 5 --> double dosing
 % MISSED = 6 --> skipped dose
 
-if DOSEFREQ == 1
+if DOSEFREQ == 1 && (MISSED == 0 || MISSED == 5 || MISSED == 6)
 y0 = [0 0 Dose]'; % Initial Conditions; units: mg/L
 options = odeset('MaxStep',5e-2, 'AbsTol', 1e-5,'RelTol', 1e-5,'InitialStep', 1e-2);
 Y1 = [];
@@ -90,7 +90,7 @@ end
 % MISSED = 3 --> taken 3m/5 h late
 % MISSED = 4 --> taken 4m/5 h late
 
-if DOSEFREQ == 2
+if DOSEFREQ == 1 && (MISSED == 1 || MISSED == 2 || MISSED == 3 || MISSED == 4)
 y0 = [0 0 p.Dose]'; % Initial Conditions; units: mg/L
 options = odeset('MaxStep',5e-2, 'AbsTol', 1e-5,'RelTol', 1e-5,'InitialStep', 1e-2);
 Y1 = [];
@@ -147,6 +147,27 @@ R = (100.*Y1(:,1))./(IC50+Y1(:,1)); %Percent receptors occupied based on IC50
 E = (100.*Y1(:,1))./(Kd+Y1(:,1)); %Percent receptors occupied based on Kd
 P_tonic = alpha1.*E+beta1; %Protection from tonic seizures based on receptor occupancy
 P_clonic = alpha2.*E+beta2; %Protection from clonic seizures based on receptor occupancy
+%P_tonicR = alpha1.*R+beta1;
+%P_clonicR = alpha2.*R+beta2;
+
+%% Updating negative values and normalizing
+ind = P_tonic<0;
+P_tonic(ind) = 0;
+P_tonic = P_tonic./(1.2922);
+
+ind2 = P_clonic<0;
+P_clonic(ind2) = 0;
+P_clonic = P_clonic./(1.5294);
+
+%{
+ind = P_tonicR<0;
+P_tonicR(ind) = 0;
+P_tonicR = P_tonicR./(1.2922);
+
+ind2 = P_clonicR<0;
+P_clonicR(ind2) = 0;
+P_clonicR = P_clonicR./(1.5294);
+%}
 
 %% MASS BALANCE
 InitialDrug = Dose;
@@ -160,7 +181,34 @@ if mean(BalanceD1)>1e-6
     disp(BalanceD1);
 end
 
-% calculate AUC by integrating the concentration curve (trapezoidal rule)
+%% calculate AUEC by integrating the protection against tonic seizures curve (trapezoidal rule)
+AUEC_tonic = 0;
+%AUEC_tonicR = 0;
+for i=1:(length(P_tonic)-1)
+    AUEC_tonic = AUEC_tonic + 0.5*(P_tonic(i,1)+P_tonic(i+1,1))*(T1(i+1)-T1(i));
+    %AUEC_tonicR = AUEC_tonicR + 0.5*(P_tonicR(i,1)+P_tonicR(i+1,1))*(T1(i+1)-T1(i));
+end
+
+%Calculate Ctrough
+E_tonic_trough = P_tonic(length(P_tonic), 1);
+%E_tonic_troughR = P_tonicR(length(P_tonicR), 1);
+
+
+%% calculate AUEC by integrating the protection against clonic seizures curve (trapezoidal rule)
+AUEC_clonic = 0;
+%AUEC_clonicR = 0;
+for i=1:(length(P_tonic)-1)
+    AUEC_clonic = AUEC_clonic + 0.5*(P_clonic(i,1)+P_clonic(i+1,1))*(T1(i+1)-T1(i));
+    %AUEC_clonicR = AUEC_clonicR + 0.5*(P_clonicR(i,1)+P_clonicR(i+1,1))*(T1(i+1)-T1(i));
+
+end
+
+%Calculate Ctrough
+E_clonic_trough = P_clonic(length(P_clonic), 1);
+%E_clonic_troughR = P_clonicR(length(P_clonicR), 1);
+
+
+%% calculate AUC by integrating the concentration curve (trapezoidal rule)
 AUC = 0;
 for i=1:(length(Y1)-1)
     AUC = AUC + 0.5*(Y1(i,1)+Y1(i+1,1))*(T1(i+1)-T1(i));
