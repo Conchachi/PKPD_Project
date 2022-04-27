d8005c8082f7b7ccbbd58aa0cf5a24a11a3a3b46
GohtaAihara <32276077+GohtaAihara@users.noreply.github.com>
2022-04-27 01:14:03 -0400

    Add files via upload

---
+++ LEV_missed_dose_consecutive.m
@@ -0,0 +1,87 @@
+clear all;
+close all;
+
+%% Missed Dose Analysis
+
+% Repeated doses, 400 mg every 12 hours
+% PARAMETERS
+q = 0;     % units: nmol/hr
+V = 21.9; % units: L (volume of distribution)
+kA  =  3.83; % units: 1/hr (absorption rate constant)
+kCL = 0.113; %units: 1/hr (clearance rate constant)
+Dose = 400; %mg
+TimeLen = 12; %hours between doses
+MASS_BAL_VIS = 0; %Set to 1 to visualize mass balance
+DOSEFREQ = 1; %Set to 0 for single dose, 1 for repeated dosing
+IC50 = 2.43; %mg/L
+Kd = 1.3617; % units: mg/L
+
+% Choose dose to miss 
+MISSED = 0; 
+% 0 = no missed doses 
+% 1 = 1 missed dose
+% 2 = 2 consecutive missed dose
+% 3 = 3 consecutive missed dose
+% 4 = 4 consecutive missed dose
+
+Conc_missed = [];
+Receptor_missed = [];
+Effect_missed = [];
+Ptonic_missed = [];
+Pclonic_missed = [];
+
+%Missed doses
+for MISSED = 1:4
+[y_m,t_m,auc_m(MISSED),ctrough_m(MISSED),receptor_m,effect_m,P_tonic_m,P_clonic_m,AUEC_tonic(MISSED) ,E_tonic_trough(MISSED) ,AUEC_clonic(MISSED) ,E_clonic_trough(MISSED)] = Levetiracetam_sim_missed_dose_consecutive(kA,V,kCL,Dose,TimeLen,q,IC50,Kd,0,1,MISSED);
+
+% Calculate Cmin and Cmax for missed doses
+y_trough = y_m(240:length(y_m));
+ptonic_trough = P_tonic_m(240:length(P_tonic_m));
+pclonic_trough = P_clonic_m(240:length(P_clonic_m));
+Cmin(MISSED) = min(y_trough, [], 'all');
+Cmax(MISSED) = max(y_m, [], 'all');
+Tonic_min(MISSED) = min(ptonic_trough, [], 'all');
+Tonic_max(MISSED) = max(P_tonic_m, [], 'all');
+Clonic_min(MISSED) = min(pclonic_trough, [], 'all');
+Clonic_max(MISSED) = max(P_clonic_m, [], 'all');
+
+%% Save concentration and effect data for each missed dose case
+Conc_missed = [Conc_missed y_m];
+Receptor_missed = [Receptor_missed receptor_m];
+Effect_missed = [Effect_missed effect_m];
+Ptonic_missed = [Ptonic_missed P_tonic_m];
+Pclonic_missed = [Pclonic_missed P_clonic_m];
+end
+
+%Print Cmin, Cmax, and AUCs to compare
+% 1 missed dose, 2 missed dose, 3 missed dose, 4 missed dose
+Cmin
+Cmax
+auc_m
+Tonic_min
+Tonic_max
+AUEC_tonic
+Clonic_min
+Clonic_max
+AUEC_clonic
+
+%% Save data for missed dosing to import into R
+
+% Columns correspond with 1 missed dose, 2 missed dose, 3 missed dose, 
+% 4 missed dose
+
+save missed_dose_consecutive_data/MissedDoseConsecutiveConc.mat Conc_missed;
+t_m = t_m'; % optimize for R visualization
+save missed_dose_consecutive_data/MissedDoseConsecutiveTime.mat t_m;
+save missed_dose_consecutive_data/MissedDoseConsecutiveAUC.mat auc_m;
+save missed_dose_consecutive_data/MissedDoseConsecutiveCtrough.mat ctrough_m;
+save missed_dose_consecutive_data/MissedDoseConsecutiveReceptor.mat Receptor_missed;
+save missed_dose_consecutive_data/MissedDoseConsecutiveEffect.mat Effect_missed;
+save missed_dose_consecutive_data/MissedDoseConsecutiveP_tonic.mat Ptonic_missed;
+save missed_dose_consecutive_data/MissedDoseConsecutiveP_clonic.mat Pclonic_missed;
+save missed_dose_consecutive_data/MissedDoseConsecutiveAUEC_tonic.mat AUEC_tonic;
+save missed_dose_consecutive_data/MissedDoseConsecutiveAUEC_clonic.mat AUEC_clonic;
+save missed_dose_consecutive_data/MissedDoseConsecutiveE_tonic_trough.mat E_tonic_trough;
+save missed_dose_consecutive_data/MissedDoseConsecutiveE_clonic_trough.mat E_clonic_trough;
+
+
---
+++ Levetiracetam_sim_missed_dose_consecutive.m
@@ -0,0 +1,213 @@
+function [Conc,Time,AUC,Ctrough, R, E, P_tonic, P_clonic,AUEC_tonic,E_tonic_trough,AUEC_clonic,E_clonic_trough] = Levetiracetam_sim_missed_dose_consecutive(kA, V, kCL, Dose, TimeLen, q, IC50, Kd, MASS_BAL_VIS, DOSEFREQ, MISSED);
+
+%% PARAMETERS
+p.q = 0;     % units: nmol/hr
+p.V = V; % units: L (volume of distribution)
+p.kA  =  kA; % units: 1/hr (absorption rate constant
+p.kCL = kCL; %units: 1/hr (clearance rate constant)
+p.Dose = Dose;
+alpha1 = 2.8366;
+beta1 = -154.44;
+alpha2 = 3.2205;
+beta2 = -169.11;
+
+y0 = [0 0 Dose]'; 
+% y0(1) = concentration in central compartment
+% y0(2) = amount cleared
+% y0(3) = amount in gut 
+
+%% SIMULATIONS
+
+%% Single dose
+if DOSEFREQ == 0
+y0 = [0 0 Dose]'; % Initial Conditions; units: mg/L
+options = odeset('MaxStep',5e-2, 'AbsTol', 1e-5,'RelTol', 1e-5,'InitialStep', 1e-2);
+[T1,Y1] = ode45(@Levetiracetam_eqns,[0 TimeLen],y0,options,p); % simulate model
+CurrentD1 = Y1(:,1)*p.V + Y1(:,3) ; % Total drug amount in system
+Conc = Y1;
+Time = T1;
+DrugIn = 0;
+end
+
+%% Repeated dosing (also double dosing and skipped dose)
+% MISSED = 0 -> no missed doses 
+% MISSED = 1 -> 1 missed dose
+% MISSED = 2 -> 2 consecutive missed dose
+% MISSED = 3 -> 3 consecutive missed dose
+% MISSED = 4 -> 4 consecutive missed dose
+
+if DOSEFREQ == 1
+    y0 = [0 0 Dose]'; % Initial Conditions; units: mg/L
+    options = odeset('MaxStep',5e-2, 'AbsTol', 1e-5,'RelTol', 1e-5,'InitialStep', 1e-2);
+    Y1 = [];
+    DrugIn = [];
+
+    %Repeated dosing
+    for i = 1:10
+        [T,Ystep] = ode45(@Levetiracetam_eqns,[0:TimeLen/240:TimeLen-TimeLen/240],y0,options,p); % simulate model
+        Y1 = [Y1; Ystep];
+        y0 = Ystep(length(Ystep),:); % Initial Conditions; units: nM
+        y0(3) = y0(3) + Dose;
+
+        %Calculating DrugIn depending on time step 
+        DrugAdded = ones(length(Ystep),1)*(i-1)*Dose;
+
+        %1st missed dose (4th dose is taken normaly)
+        if i == 4 && (MISSED == 1 || MISSED == 2 || MISSED == 3 || MISSED == 4)
+            y0(3) = y0(3) - Dose;
+        end
+
+        %2nd missed dose (4th dose is taken normally)
+        if i == 5 && (MISSED == 2 || MISSED == 3 || MISSED == 4)
+            y0(3) = y0(3) - Dose;
+        end
+
+        %3rd missed dose (4th dose is taken normally)
+        if i == 6 && (MISSED == 3 || MISSED == 4)
+            y0(3) = y0(3) - Dose;
+        end
+
+        %4th missed dose (4th dose is taken normally)
+        if i == 7 && (MISSED == 4)
+            y0(3) = y0(3) - Dose;
+        end
+        
+        %All doses following the 1st missed dose are 1 less
+        if i > 4 && (MISSED == 1 || MISSED == 2 || MISSED == 3 || MISSED == 4)
+            DrugAdded = ones(length(Ystep),1)*(i-2)*Dose;
+        end
+        
+        %All doses following the 2nd missed dose are 2 less
+        if i > 5 && (MISSED == 2 || MISSED == 3 || MISSED == 4)
+            DrugAdded = ones(length(Ystep),1)*(i-3)*Dose;
+        end
+        
+        %All doses following the 2nd missed dose are 3 less
+        if i > 6 && (MISSED == 3 || MISSED == 4)
+            DrugAdded = ones(length(Ystep),1)*(i-4)*Dose;
+        end
+        
+        %All doses following the 2nd missed dose are 3 less
+        if i > 7 && (MISSED == 4)
+            DrugAdded = ones(length(Ystep),1)*(i-5)*Dose;
+        end
+        
+        %Amount of drug going into system
+        DrugIn = [DrugIn; DrugAdded];
+        
+    end
+    
+    T1 = [0:TimeLen/240:TimeLen*10-TimeLen/240]; %Total time
+    CurrentD1 = Y1(:,1)*p.V + Y1(:,3); % Total drug amount in system
+    Conc = Y1(:,1);
+    Time = T1;
+    
+end
+%% Effect models for receptor occupancy
+R = (100.*Y1(:,1))./(IC50+Y1(:,1)); %Percent receptors occupied based on IC50
+E = (100.*Y1(:,1))./(Kd+Y1(:,1)); %Percent receptors occupied based on Kd
+P_tonic = alpha1.*E+beta1; %Protection from tonic seizures based on receptor occupancy
+P_clonic = alpha2.*E+beta2; %Protection from clonic seizures based on receptor occupancy
+%P_tonicR = alpha1.*R+beta1;
+%P_clonicR = alpha2.*R+beta2;
+
+%% Updating negative values and normalizing
+ind = P_tonic<0;
+P_tonic(ind) = 0;
+P_tonic = P_tonic./(1.2922);
+
+ind2 = P_clonic<0;
+P_clonic(ind2) = 0;
+P_clonic = P_clonic./(1.5294);
+
+%{
+ind = P_tonicR<0;
+P_tonicR(ind) = 0;
+P_tonicR = P_tonicR./(1.2922);
+
+ind2 = P_clonicR<0;
+P_clonicR(ind2) = 0;
+P_clonicR = P_clonicR./(1.5294);
+%}
+
+%% MASS BALANCE
+InitialDrug = Dose;
+DrugOut = Y1(:,2);
+BalanceD1 = DrugIn - DrugOut - CurrentD1 + InitialDrug; %(zero = balance)
+
+% Add an automated check/report on mass balance, since we don't want to 
+% look at hundreds of mass balance graphs
+if mean(BalanceD1)>1e-6
+    disp('Mass imbalance possible: ');
+    disp(BalanceD1);
+end
+
+%% calculate AUEC by integrating the protection against tonic seizures curve (trapezoidal rule)
+AUEC_tonic = 0;
+%AUEC_tonicR = 0;
+for i=1:(length(P_tonic)-1)
+    AUEC_tonic = AUEC_tonic + 0.5*(P_tonic(i,1)+P_tonic(i+1,1))*(T1(i+1)-T1(i));
+    %AUEC_tonicR = AUEC_tonicR + 0.5*(P_tonicR(i,1)+P_tonicR(i+1,1))*(T1(i+1)-T1(i));
+end
+
+%Calculate Ctrough
+E_tonic_trough = P_tonic(length(P_tonic), 1);
+%E_tonic_troughR = P_tonicR(length(P_tonicR), 1);
+
+
+%% calculate AUEC by integrating the protection against clonic seizures curve (trapezoidal rule)
+AUEC_clonic = 0;
+%AUEC_clonicR = 0;
+for i=1:(length(P_tonic)-1)
+    AUEC_clonic = AUEC_clonic + 0.5*(P_clonic(i,1)+P_clonic(i+1,1))*(T1(i+1)-T1(i));
+    %AUEC_clonicR = AUEC_clonicR + 0.5*(P_clonicR(i,1)+P_clonicR(i+1,1))*(T1(i+1)-T1(i));
+
+end
+
+%Calculate Ctrough
+E_clonic_trough = P_clonic(length(P_clonic), 1);
+%E_clonic_troughR = P_clonicR(length(P_clonicR), 1);
+
+
+%% calculate AUC by integrating the concentration curve (trapezoidal rule)
+AUC = 0;
+for i=1:(length(Y1)-1)
+    AUC = AUC + 0.5*(Y1(i,1)+Y1(i+1,1))*(T1(i+1)-T1(i));
+end
+
+Ctrough = Y1(length(Y1), 1);
+
+
+%% VISUALIZATION OF MASS BALANCE
+
+if MASS_BAL_VIS == 1
+%Concentration of Lev in central comp
+figure; 
+ax1=subplot(2,2,1); 
+plot(ax1,T1,Y1(:,1),'linewidth',3)
+title(ax1,'Concentration of Levetiracetam in Compartment')
+ylabel(ax1,'[D] (mg/L)')
+xlabel(ax1,'Time (hrs)')
+
+%Amount of drug cleared
+ax2=subplot(2,2,2);
+plot(ax2,T1,Y1(:,2),'linewidth',3)
+title(ax2,'Total Amount of Drug Cleared')
+ylabel(ax2,'Total Drug (mg)')
+xlabel(ax2,'Time (hrs)')
+
+%Amount of drug in gut
+ax3=subplot(2,2,3);
+plot(ax3,T1,Y1(:,3),'linewidth',3)
+title(ax3,'Total Amount of Drug In Gut')
+ylabel(ax3,'Total Drug (mg)')
+xlabel(ax3,'Time (hrs)')
+
+%Mass balance
+ax4=subplot(2,2,4);
+plot(ax4,T1,BalanceD1,'linewidth',3)
+title(ax4,'Mass/molecular balance for the drug')
+ylabel(ax4,'Balance of drug (mg)')
+xlabel(ax4,'Time (hrs)')
+end
+
