af57cbf823beb274f30b172c0ed9fe71eb4d3f25
kaitlynstorm <65556252+kaitlynstorm@users.noreply.github.com>
2022-04-27 22:46:02 -0400

    Update LEV_Sens_Analysis.R

--- LEV_Sens_Analysis.R
+++ LEV_Sens_Analysis.R
@@ -9,14 +9,17 @@ library("plotly")
 library("shiny")
 
 # set theme for all plots
-theme1 <- theme_classic() +
-  theme(text = element_text(size = 20),
-        plot.title = element_text(hjust = 0),
-        axis.title.x = element_text(margin = margin(10, 0, 0, 0)),
-        axis.title.y = element_text(margin = margin(0, 10, 0, 0)),
-        axis.text.x = element_text(margin = margin(5, 0, 0, 0)),
-        axis.text.y = element_text(margin = margin(0, 5, 0, 0)),
+theme1 <- theme_classic(base_size = 20) +
+  theme(text = element_text(),
+        # plot.title = element_text(hjust = 0),
+        # axis.title.x = element_text(margin = margin(10, 0, 0, 0)),
+        # axis.title.y = element_text(margin = margin(0, 10, 0, 0)),
+        # axis.text.x = element_text(margin = margin(5, 0, 0, 0)),
+        # axis.text.y = element_text(margin = margin(0, 5, 0, 0)),
+        panel.grid.major.y = element_line(colour = "grey", size = 0.2),
+        panel.grid.minor.y = element_line(colour = "grey", size = 0.2),
         plot.title.position = "plot")
+        
 
 
 SensAUC <- readMat('SensAUC.mat')
@@ -41,15 +44,15 @@ SensCtrough <- as.data.frame(SensCtrough)
 names(SensAUC) <- c('kA,AUC', 'V,AUC', 'kCL,AUC', 'Dose,AUC', 'TimeLen,AUC')
 names(SensClonicAUEC) <- c('kA,AUEC - Clonic', 'V,AUEC - Clonic', 'kCL,AUEC - Clonic', 'Dose,AUEC - Clonic', 
                            'TimeLen,AUEC - Clonic')
-names(SensClonicEtrough) <- c('kA,Effective Trough - Clonic', 'V,Effective Trough - Clonic', 
-                              'kCL,Effective Trough - Clonic', 'Dose,Effective Trough - Clonic', 
-                              'TimeLen,Effective Trough - Clonic')
-names(SensCtrough) <- c('kA,Concentration Trough', 'V,Concentration Trough', 'kCL,Concentration Trough', 
-                        'Dose,Concentration Trough', 'TimeLen,Concentration Trough')
+names(SensClonicEtrough) <- c('kA,Clonic E_Trough', 'V,Clonic E_Trough', 
+                              'kCL,Clonic E_Trough', 'Dose,Clonic E_Trough', 
+                              'TimeLen,Clonic E_Trough')
+names(SensCtrough) <- c('kA,C_Trough', 'V,C_Trough', 'kCL,C_Trough', 
+                        'Dose,C_Trough', 'TimeLen,C_Trough')
 names(SensTonicAUEC) <- c('kA,AUEC - Tonic', 'V,AUEC - Tonic', 'kCL,AUEC - Tonic', 'Dose,AUEC - Tonic', 'TimeLen,AUEC - Tonic')
-names(SensTonicEtrough) <- c('kA,Effective Trough - Tonic', 'V,Effective Trough - Tonic',
-                             'kCL,Effective Trough - Tonic', 'Dose,Effective Trough - Tonic',
-                             'TimeLen,Effective Trough - Tonic')
+names(SensTonicEtrough) <- c('kA,Tonic E_Trough', 'V,Tonic E_Trough',
+                             'kCL,Tonic E_Trough', 'Dose,Tonic E_Trough',
+                             'TimeLen,Tonic E_Trough')
 
 
 AUC <- cbind(SensAUC, SensClonicAUEC, SensTonicAUEC)
@@ -67,24 +70,28 @@ Trough <- pivot_longer(Trough,
 
 AUC <- separate(AUC,
                col = "Parameters",
-               into = c("Parameter", "Relative_Sensitivity_of"),
+               into = c("Parameter", "Legend"),
                sep = ",")
 
 Trough <- separate(Trough,
                 col = "Parameters",
-                into = c("Parameter", "Relative_Sensitivity_of"),
+                into = c("Parameter", "Legend"),
                 sep = ",")
 
-AUC_plot<-ggplot(data = AUC, aes(x=Parameter, y=Sensitivity, colour = Relative_Sensitivity_of, fill=Relative_Sensitivity_of)) +
+AUC_plot<-ggplot(data = AUC, aes(x=Parameter, y=Sensitivity, colour = Legend, fill=Legend)) +
   geom_bar(stat="identity", position=position_dodge()) + 
   labs(title = 'A')+
-  ylim(-1.8,1.8) 
+  ylim(-1.8,1.8)  + 
+  theme1
 AUC_plot
-ggsave(filename = 'AUC_plot.png', plot = AUC_plot, width = 8, height = 6)
+ggsave(filename = 'LocalSens_AUC_plot.png', plot = AUC_plot, width = 8, height = 6)
 
-Trough_plot<-ggplot(data = Trough, aes(x=Parameter, y=Sensitivity, colour = Relative_Sensitivity_of, fill=Relative_Sensitivity_of)) +
+Trough_plot<-ggplot(data = Trough, aes(x=Parameter, y=Sensitivity, colour = Legend, fill=Legend)) +
   geom_bar(stat="identity", position=position_dodge()) + 
   labs(title = 'B')+
-  ylim(-1.8,1.8) 
+  ylim(-1.8,1.8) +
+  theme1
 Trough_plot
-ggsave(filename = 'Trough_plot.png', plot = Trough_plot, width = 8, height = 6)
+ggsave(filename = 'LocalSens_Trough_plot.png', plot = Trough_plot, width = 8, height = 6)
+
+
