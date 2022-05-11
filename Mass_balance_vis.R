4710a64fba5aece60e57c16f82eb6cc59e60ad6f
kaitlynstorm <65556252+kaitlynstorm@users.noreply.github.com>
2022-05-11 10:11:56 -0400

    Update Mass_balance_vis.R
    Previously Fig S1, now Fig 2

--- Mass_balance_vis.R
+++ Mass_balance_vis.R
@@ -20,8 +20,8 @@ names(MassBal_Rep) <- col_names
 
 #Create theme for ggplot
 theme1 <- theme_classic() +
-  theme(text = element_text(size = 18),
-        plot.title = element_text(hjust = 0.5),
+  theme(text = element_text(size = 28),
+        #plot.title = element_text(hjust = 0.5),
         axis.title.x = element_text(margin = margin(10, 0, 0, 0)),
         axis.title.y = element_text(margin = margin(0, 10, 0, 0)),
         axis.text.x = element_text(margin = margin(5, 0, 0, 0)),
@@ -33,7 +33,7 @@ theme1 <- theme_classic() +
 #Plot data using ggplot
 MassBal_Single_Plot <- ggplot(data = NULL) +
   geom_line(data = MassBal_Single, aes(x = Time, y = Mass_Bal), color = 'blue1') +
-  labs(title = 'Mass Balance: Single Dose') +
+  labs(title = 'A') +
   scale_x_continuous(name = 'Time (h)') +
   scale_y_continuous(name = 'Mass Balance (mg/L)') +
   theme1 
@@ -41,7 +41,7 @@ MassBal_Single_Plot <- ggplot(data = NULL) +
 #Plot data using ggplot
 MassBal_Rep_Plot <- ggplot(data = NULL) +
   geom_line(data = MassBal_Rep, aes(x = Time, y = Mass_Bal), color = 'blue1') +
-  labs(title = 'Mass Balance: Repeated Dosing') +
+  labs(title = 'B') +
   scale_x_continuous(name = 'Time (h)') +
   scale_y_continuous(name = 'Mass Balance (mg/L)') +
   theme1 
@@ -49,6 +49,6 @@ MassBal_Rep_Plot <- ggplot(data = NULL) +
 print(MassBal_Single_Plot)
 print(MassBal_Rep_Plot)
 
-
-
+ggsave(filename = 'MassBal_Single_Plot.png', plot = MassBal_Single_Plot, width = 8.5, height = 6)
+ggsave(filename = 'MassBal_Rep_Plot.png', plot = MassBal_Rep_Plot, width = 9, height = 6)
 
